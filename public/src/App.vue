464427d273513f6e6450c2e1d5788a629da877b7
Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>
2023-03-17 01:25:54 +0200

    added a hamburger menu
    Signed-off-by: Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>

--- public/src/App.vue
+++ public/src/App.vue
@@ -1,25 +1,31 @@
 
 <template>
+  <Menu />
   <div class="common-layout">
     <el-container>
-      <el-header><h1><img style="width: 40px;position: relative;top: 10px;left: -10px;" src="./assets/logo.png">Smart Parks Connect</h1></el-header>
+      <el-header>
+        <h1><img style="width: 40px;position: relative;top: 10px;left: -10px;" src="./assets/logo.png">Smart Parks Connect
+        </h1>
+      </el-header>
       <el-main>
-        <Form/>
+        <Form />
       </el-main>
     </el-container>
   </div>
 </template>
 <style>
-h1{
+h1 {
   text-align: center;
 }
 </style>
 <script>
 import Form from './components/Form.vue'
+import Menu from './components/Menu.vue';
 
 export default {
   components: {
-    Form
+    Form,
+    Menu
   }
 }
 </script>
