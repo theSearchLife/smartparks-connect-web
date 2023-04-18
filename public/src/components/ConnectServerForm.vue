02c1ce91429160bad903fb0460a3fc6db64d750a
Branko Šuligoj <branko.suligoj@mobik.com>
2023-04-18 21:16:40 +0200

    Implemented autocomplete for Server URL

--- public/src/components/ConnectServerForm.vue
+++ public/src/components/ConnectServerForm.vue
@@ -8,45 +8,51 @@
       </div>
     </template>
 
-    <el-form :model="formServer" :inline="true" ref="formSRef">
+    <el-form :model="formServer" status-icon inline ref="formSRef" label-width="150px">
       <el-row>
-        <el-col span=24>
-          <el-form-item label="Device Template:" prop="device_template" :rules="{
-            required: true,
-            message: 'device template can not be null',
-            trigger: 'blur',
-          }">
-            <el-select v-model="formServer.device_template" class="m-2" placeholder="Select" @change="changeTemplate"
-              style="width: 200px;">
-              <el-option v-for="item, key in deviceTemplates" :key="key" :label="key" :value="key" />
-            </el-select>
-          </el-form-item>
-        </el-col>
+        <el-form-item label="Device Template" :width="500" prop="device_template" :rules="{
+          required: true,
+          message: 'Device Template is required',
+          trigger: 'blur',
+        }">
+          <el-select v-model="formServer.device_template" class="m-2" placeholder="Select" @change="changeTemplate"
+            style="width: 350px;">
+            <el-option v-for="item, key in deviceTemplates" :key="key" :label="key" :value="key" />
+          </el-select>
+        </el-form-item>
       </el-row>
-      <el-form-item label="Server address" width="200" prop="server_url" :rules="{
-        validator: validateGrpcHostPort,
-        trigger: 'blur',
-      }">
-        <el-input v-model="formServer.server_url" placeholder="host:port" autocomplete="on" />
-      </el-form-item>
 
 
-      <el-form-item label="ApiKey" prop="api_key" :rules="{
-        required: true,
-        message: 'api_key can not be null',
-        trigger: 'blur',
-      }">
-        <el-input v-model="formServer.api_key" type="password"></el-input>
-      </el-form-item>
+      <el-row>
+        <el-form-item label="Server Address" prop="server_url" :rules="{
+          validator: validateGrpcHostPort,
+          required: true,
+          trigger: 'blur',
+        }">
+          <el-autocomplete v-model="formServer.server_url" name="serverURL" placeholder="host:port"
+            :fetch-suggestions="querySearch" @select="handleSelect" style="width: 350px;" />
+        </el-form-item>
+      </el-row>
 
-      <el-form-item>
-        <el-popover placement="bottom" title="Get ApiKey" :width="200" trigger="hover"
-          content="Click this button to obtain a valid ApiKey using your server email and password">
-          <template #reference>
-            <el-button type="danger" :icon="Key" @click="config.dialogFormVisible = true">Get ApiKey</el-button>
-          </template>
-        </el-popover>
-      </el-form-item>
+      <el-row>
+        <el-form-item label="API Key" prop="api_key" :rules="{
+          required: true,
+          message: 'API Key is required',
+          trigger: 'blur',
+        }">
+          <el-input v-model="formServer.api_key" type="password" show-password style="width: 350px;"></el-input>
+        </el-form-item>
+
+        <el-form-item>
+          <el-popover placement="bottom" title="Get API Key" :width="200" trigger="hover"
+            content="Click this button to obtain a valid API key using your server email and password">
+            <template #reference>
+              <el-button type="danger" :icon="Key" @click="config.dialogFormVisible = true">Get ApiKey</el-button>
+            </template>
+          </el-popover>
+        </el-form-item>
+      </el-row>
+
 
       <el-row>
         <el-form-item>
@@ -63,20 +69,20 @@
 
   </el-card>
 
-  <el-dialog v-model="config.dialogFormVisible" title="Get APIKey By email and password">
+  <el-dialog v-model="config.dialogFormVisible" title="Get API Key By email and password">
     <el-form :model="formLogin">
-      <el-form-item label="email">
-        <el-input v-model="formLogin.email" autocomplete="off" />
+      <el-form-item label="Email" required>
+        <el-input v-model="formLogin.email" />
       </el-form-item>
-      <el-form-item label="password">
-        <el-input type="password" v-model="formLogin.password" autocomplete="off" />
+      <el-form-item label="Password" required>
+        <el-input type="password" v-model="formLogin.password" autocomplete="off" show-password />
       </el-form-item>
     </el-form>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="config.dialogFormVisible = false">Cancel</el-button>
         <el-button type="primary" @click="userLogin">
-          GetApiKey
+          Get API Key
         </el-button>
       </span>
     </template>
@@ -84,26 +90,28 @@
 </template>
 
 <script lang="ts" setup>
+import { getSet, ladd, lget, lsave } from '@/js/localstore';
+import { request } from "@/js/request";
+import { Key } from '@element-plus/icons-vue';
+import type { FormInstance } from 'element-plus';
+import { filter } from 'lodash';
+import { reactive, ref, watch } from 'vue';
 import ClearCacheButton from './ClearCacheButton.vue';
-import { reactive, VueElement, ref, watch, onBeforeMount } from 'vue'
-import { request } from '../js/request'
-import { CascaderProps, popoverEmits } from 'element-plus'
-import { Key, User } from '@element-plus/icons-vue'
-import type { FormInstance, FormRules } from 'element-plus'
-import { lsave, lget, getSet } from '../js/localstore'
 const emit = defineEmits(["connectServer"]);
 const formSRef = ref<FormInstance>()
 const deviceTemplates = reactive({})
-var validateGrpcHostPort = (rule, value, callback) => {
+
+const validateGrpcHostPort = (_, value, callback) => {
   let re = /^[a-zA-Z\.\-0-9]{0,}:\d{1,5}$/
   if (value === '' || value == null) {
-    callback(new Error('Please input the grpc host:port'));
+    callback(new Error('GRPC host:port is required'));
   } else if (!re.test(value)) {
-    callback(new Error('Format must be:  host:port (without any protocol prefix)'));
+    callback(new Error('Format must be: host:port (without any protocol prefix)'));
   } else {
     callback();
   }
 };
+
 request('v1/template/list', "GET").then((resp) => {
   for (let i in resp) {
     deviceTemplates[i] = resp[i]
@@ -116,13 +124,14 @@ request('v1/template/list', "GET").then((resp) => {
 
 
 const formServer = reactive({
-  device_template: "",
+  device_template: '',
   server_url: '',
   api_key: ''
 })
 const formLogin = reactive({
-  email: "",
-  password: "",
+  server_url: '',
+  email: '',
+  password: '',
 })
 
 const initModel = () => {
@@ -166,6 +175,7 @@ const connectServerSubmit = (formEl) => {
           orgList.push({ "id": resp[i].id, "name": resp[i].name })
         }
         config.connected = true
+        ladd("connectedServers", formServer)
         emit('connectServer', formServer, config, orgList, deviceTemplates)
       }, (err) => {
         alert(err)
@@ -174,6 +184,27 @@ const connectServerSubmit = (formEl) => {
       return false
     }
   })
+}
+
+const querySearch = (queryString: string, cb: any) => {
+  console.log(queryString)
+  const results = filter(lget("connectedServers"), createFilter(queryString))
+  // call callback function to return suggestions
+  cb(results)
+}
+const createFilter = (queryString: string) => {
+  return (item) => {
+    return (
+      item.server_url.toLowerCase().indexOf(queryString.toLowerCase()) === 0
+    )
+  }
+}
+
+const handleSelect = (item) => {
+  console.log(item)
+  formServer.api_key = item.api_key
+  formServer.device_template = item.device_template
+  formServer.server_url = item.server_url
 }
 
 </script>
--- public/src/js/localstore.js
+++ public/src/js/localstore.js
@@ -1,13 +1,34 @@
 const keyPrefix = "save_i_"
 
-
-const getKey = (key) =>{
+const getKey = (key) => {
     return keyPrefix + key
 }
-export const lsave = (key,value)=>{
-   console.log(key,value)
-   let valueText = JSON.stringify(value)
-   localStorage.setItem(getKey(key),valueText)
+
+export const lsave = (key, value) => {
+    console.log(key, value)
+    let valueText = JSON.stringify(value)
+    localStorage.setItem(getKey(key), valueText)
+}
+
+
+// Added for storing successfully connected sessions
+export const ladd = (key, value) => {
+    value.value = value.server_url
+    var currentValue = localStorage.getItem(getKey(key))
+    if (currentValue === null) {
+        lsave(key, [value])
+        return
+    }
+    currentValue = JSON.parse(currentValue)
+    if (Array.isArray(currentValue)) {
+        for (let i = 0; i < currentValue.length; i++) {
+            if (currentValue[i].value === value.value) {
+                return
+            }
+        }
+        currentValue.push(value)
+        lsave(key, currentValue)
+    }
 }
 
 export const lget = (key) => {
@@ -18,13 +39,13 @@ export const lget = (key) => {
     return JSON.parse(valueText)
 }
 
-export const getSet = (key,obj) => {
+export const getSet = (key, obj) => {
     let value = lget(key)
-    if (value != null){
-        for (var i in obj){
+    if (value != null) {
+        for (var i in obj) {
             obj[i] = value[i]
         }
-        for (var i in value){
+        for (var i in value) {
             obj[i] = value[i]
         }
     }
