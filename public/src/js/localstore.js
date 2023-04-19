3e5c34e2e7de3ca5f89213e227bb470a2eaa2f22
Branko Šuligoj <branko.suligoj@mobik.com>
2023-04-19 21:38:48 +0200

    removed storing connected form info in localstore

--- public/src/js/localstore.js
+++ public/src/js/localstore.js
@@ -10,27 +10,6 @@ export const lsave = (key, value) => {
     localStorage.setItem(getKey(key), valueText)
 }
 
-
-// Added for storing successfully connected sessions
-export const ladd = (key, value) => {
-    value.value = value.server_url
-    var currentValue = localStorage.getItem(getKey(key))
-    if (currentValue === null) {
-        lsave(key, [value])
-        return
-    }
-    currentValue = JSON.parse(currentValue)
-    if (Array.isArray(currentValue)) {
-        for (let i = 0; i < currentValue.length; i++) {
-            if (currentValue[i].value === value.value) {
-                return
-            }
-        }
-        currentValue.push(value)
-        lsave(key, currentValue)
-    }
-}
-
 export const lget = (key) => {
     let valueText = localStorage.getItem(getKey(key))
     if (valueText == "") {
